bb99920adc5c9c3e1f08977ff57146a2209374f0
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-26 10:51:00 +0300

    Add logic

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+'use strict';
+const imagesDB = [{https://oir.mobi/uploads/posts/2021-05/1622262892_61-oir_mobi-p-krasivie-vidi-prirodi-priroda-krasivo-foto-65.jpg, https://downloadwap.com/thumbs2/wallpapers/p2/2019/nature/47/c328965713368880.jpg, https://i.pinimg.com/474x/83/58/37/835837bfbf098d0826756eceecbefdd5.jpg}];
+
+const img = document.querySelector('.slider>img');
+
+const [btnPrev,btnNext] = document.querySelectorAll('.slider-container>button');
+
+console.log(img);
+console.log(btnPrev);
+console.log(btnNext);
