f6db5bd4e73a0326dd9b62b5851bc5d71c6ca7ba
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-27 09:51:12 +0300

    Add closure variable and func

---
+++ assets/js/sandbox.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// function createCounter(){
+//   let i = 0; //замкнутая переменная
+//   return function counter (){    
+//     return ++i;
+//   }
+// }
+// function createCounter(init=0){
+//   let i = init; //замкнутая переменная
+//   return {    
+//     increment(){
+//     return ++i;
+//     },
+//     decrement(){
+//       return --i;
+//       },
+//   }
+// }
+
+// const counter1 = createCounter(10);
+// console.log(counter1.increment());
+// const counter2 = createCounter();
+
