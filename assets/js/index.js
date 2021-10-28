417cb0d1bd37b15ad58d99d6159890008f622699
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-28 11:58:30 +0300

    Dipping

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,24 @@
 
 const buttons = document.querySelectorAll('button');
 
-
-const btnHandler =({target:{
-  dataset:{color},
-  parentNode: parentRoot,
-  }
-})=> parentRoot.style.backgroundColor=color;
+const handler = (e)=>{
+ e.capture= true;
+ console.dir(e.target);
+ console.dir(e.currentTarget);
+}
+// const btnHandler =({target:{
+//   dataset:{color},
+//   parentNode: parentRoot,
+//   }
+// })=> parentRoot.style.backgroundColor=color;
 
 
 for (const btn of buttons) {
-  btn.addEventListener('click', btnHandler);
+  // btn.addEventListener('click', btnHandler);
+  btn.addEventListener('click', handler);
 } 
+
+document.getElementById('root').addEventListener('click', handler);
+document.body.addEventListener('click', handler);
+document.addEventListener('click', handler);
+window.addEventListener('click', handler, true);
