79c11b83aa93212654a8213485374a26133da97e
Andrey Shapovalov <llianandr94@gmai.com>
2021-10-29 11:52:57 +0300

    Fixed img, content arrors

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,24 +11,52 @@ function createActorCards(actor){
   const container = document.createElement('article');
   container.classList.add('cardContainer');
 
+  const imgWrapper = document.createElement('div');
+  imgWrapper.classList.add('cardImageWrapper');
+
+  const initials = document.createElement('div');
+  initials.classList.add('initials');
+  initials.append(document.createTextNode(actor.name[0]|| 'noname'));
+  initials.style.backgroundColor = stringToColour(actor.name|| '');
+
   const img = document.createElement('img');
   img.classList.add('cardImage')
   img.setAttribute('src', actor.photo);
   img.setAttribute('alt', actor.name);
+  img.addEventListener('error', handleImgError)
+
+  imgWrapper.append(initials,img);
 
   const name = document.createElement('h2');
   name.classList.add('cardName');   
   // name.textContent = actor.name;
-  name.append(document.createTextNode(actor.name));
+  name.append(document.createTextNode(actor.name|| ''));
 
   const description = document.createElement('p');
   description.classList.add('cardDesctiption');
-  description.append(document.createTextNode(actor.birthdate));
+  description.append(document.createTextNode(actor.birthdate|| 'unknow'));
 
-  container.append(img, name, description);
+  container.append(imgWrapper, name, description);
   card.appendChild(container);
   return card;
 }
 
+cardsContainer.append(...HTMLElements);
 
-// cardsContainer.append(...HTMLElements);
+/* handlers */
+function handleImgError({target}){
+  target.remove();
+}
+/* utilits */
+function stringToColour(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
