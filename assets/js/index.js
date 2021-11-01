466165f05a5bd84787fbcd12ac5faf093d0f60bc
Andrey Shapovalov <llianandr94@gmail.com>
2021-11-01 12:59:25 +0200

    Add task : display flex-direction behavior

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,13 @@
 'use strict';
 
-const arrSrc = ['https://klike.net/uploads/posts/2019-12/1577611603_6.jpg','https://fishki.net/picsw/072011/15/bonus/peyzazhi/tn.jpg'];
-
-const btn = document.querySelector('button');
-const img = document.querySelector('.img-wrapper>img');
-const counter = oneFromTwo();
+const buttons = document.querySelectorAll('button');
+const wrapper = document.querySelector('.wrapper');
 
 
-const imgHandler = function(){  
-   
-  img.setAttribute('src', arrSrc[counter()]);    
+const flexHandler = ({target:{dataset:{flexDirection}}})=>{ 
+  wrapper.style.flexDirection=flexDirection;     
 };
 
-btn.addEventListener('click', imgHandler);
-
-
-function oneFromTwo(){
-  let count = 0; 
-  const innerCounter =()=>(count += 1)%2;   
-  return innerCounter;
+for (const button of buttons) {
+  button.addEventListener('click', flexHandler);  
 }
-  
