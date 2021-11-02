b855fafe8c8ad5c8e54031b1daa31a3a2cc39c10
Andrey Shapovalov <llianandr94@gmail.com>
2021-11-02 08:01:14 +0200

    Rendering elements from input form

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,101 +1,23 @@
 'use strict';
-const cardsContainer = document.getElementById('cardsContainer');
-const HTMLElements = actors
-  .filter((actor)=>actor.name&&actor.birthdate&&actor.photo)
-  .map((actor)=>createActorCards(actor));
 
-function createActorCards(actor){
-  const p = createElement('p', {classNames:['cardDescription']},
-        document.createTextNode(actor.birthdate || 'unknow')
-      );
-  const h2 = createElement('h2', {classNames:['cardName']},
-        document.createTextNode(actor.name || 'noname')
-      );
-  const article = createElement('article', {classNames:['cardContainer']}, 
-        createImageWrapper(actor),
-        h2,
-        p,
-      );
-  return createElement('li',{classNames:['cardWrapper']}, article);
-}
-cardsContainer.append(...HTMLElements);
+const messageArray = [];
+const list = document.getElementById('list');
+const form = document.getElementById('root-form');
 
-/**
- * 
- * @param {string} type 
- * @param {object} options 
- * @param {string} options.typeEvent
- * @param {string[]} options.classNames
- * @param {function} options.onClick
- * @param {Node[]} children 
- * return {Node}
- */
-function createElement(type,{classNames, typeEvent, onClick}, ...children){
-  const elem = document.createElement(type);
-  elem.classList.add(...classNames);
-  elem.addEventListener(typeEvent, onClick);//for example
-  elem.append(...children);
-  return elem;
-}
-
-function createImageWrapper(actor){
-  const {id, name} = actor;
-  const imgWrapper = document.createElement('div');
-  imgWrapper.setAttribute('id', `wrapper${id}`);
-  imgWrapper.classList.add('cardImageWrapper');
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  const {target: { textInput }} = e;
+  //после нажатия на кнопку добавлять значение инпута в массив, если оно не пустое
+  const inputValue = textInput.value.trim();
+  if(inputValue){
+    messageArray.push(inputValue);    
+    const item = document.createElement('li');
+    item.innerText= inputValue;
+    list.append(item);
+    //после добавления - очищать форму
+    form.reset();
+  }
   
-  const initials = document.createElement('div');
-  initials.classList.add('initials');
-  initials.append(document.createTextNode(getFirstLetterInitials(name) || 'noname'));  
-  initials.style.backgroundColor = stringToColour(name || '');
-
-  imgWrapper.append(initials);
-  createImage(actor);
-  return imgWrapper;
-}
-
-function createImage({photo, name, id}){
-  const img = document.createElement('img');
-  img.dataset.id = id;  
-  img.classList.add('cardImage');
-  img.setAttribute('src', photo);
-  img.setAttribute('alt', name);
-  img.addEventListener('error', handleImageError);
-  img.addEventListener('load', handleImageLoad);
-  //return img;
-}
-
-/*  handles */
-function handleImageError({target}){
-  target.remove();
-}
-function handleImageLoad({
-  target, 
-  target:{
-    dataset:{id},
-  }
-}){
-  document.getElementById(`wrapper${id}`).append(target);
-}
-
-/* utilits */
-function stringToColour(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  let colour = '#';
-  for (let i = 0; i < 3; i++) {
-    let value = (hash >> (i * 8)) & 0xFF;
-    colour += ('00' + value.toString(16)).substr(-2);
-  }
-  return colour;
-}
-
-function getFirstLetterInitials (str){  
-  let newStr = '';
-  for (const element of str.split(' ')) {
-    newStr += element.slice(0,1).toUpperCase();
-  }
-  return newStr;
-}
+  //рендерить на странице то, что добавилось в массив
+  console.dir(messageArray);
+})
