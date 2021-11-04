338527e407596245f145663f8a252aaeeb4a3640
Andrey Shapovalov <llianandr94@gmai.com>
2021-11-04 12:01:13 +0200

    Add JSON

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,5 @@
 'use strict';
 
-const messageArray = [];
-const list = document.getElementById('list');
-const form = document.getElementById('root-form');
-
-form.addEventListener('submit', (e)=>{
-  e.preventDefault();
-  const {target: { textInput }} = e;
-  //после нажатия на кнопку добавлять значение инпута в массив, если оно не пустое
-  const inputValue = textInput.value.trim();    
-  if(inputValue){    
-    messageArray.push(inputValue); 
-    list.append(createElement('li', inputValue));       
-    //после добавления - очищать форму
-    form.reset();
-  }  
-  //рендерить на странице то, что добавилось в массив
+function loadImage(){
   
-})
- function createElement(type, text){
-   const elem = document.createElement(type);
-   elem.append(document.createTextNode(text));
-   return elem;
- }
-
-
-//  const dateValidator= /^([12][09]\d{2})\/([01]\d)\/([0-3]\d)$/;
-//  // осталось ограничить в $1 между 1941-2021..
-
-const fileNameValidator = /^([^\\\/:*?"<>|])+\.((jpg)|(png))$/;
-
-function counter(){
-  let i=1;
-  const id = setInterval(() => {
-    console.log(i++);
-        if(i===11){
-      clearInterval(id);
-    }
-  }, 500);
 }
-
-function start(counter=0){
-  if(counter < 10){
-    setTimeout(function(){
-      counter++;
-      console.log(counter);
-      start(counter);
-    }, 500);
-  }
-}
-start();
