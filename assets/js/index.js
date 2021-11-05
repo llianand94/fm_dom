e5082e660a177bc3ae00b2ac909cdc62db1fb746
Andrey Shapovalov <llianandr94@gmail.com>
2021-11-05 08:09:42 +0200

    Task Json & Promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
-function loadImage(){
+const promiseObj = fetch('./assets/js/fruits.json').then((response)=>response.json()).then((data)=> {
+  data.forEach((obj)=>{console.log(obj.fruit)});
   
-}
+  const onlyRed = data.filter((fruit)=>fruit.color==='Red');
+  console.log('Array only Red fruits', onlyRed);
+
+  const onlyLarge = data.filter((fruit)=>fruit.size==='Large');
+  console.log('Array only', onlyLarge);
+});
+
