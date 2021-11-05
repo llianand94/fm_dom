d23c15f58b405fa7c7f0b21e0770d7cce5c58e0e
Andrey Shapovalov <llianandr94@gmai.com>
2021-11-05 09:47:43 +0200

    Accurate point of task JSON & Promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const promiseObj = fetch('./assets/js/fruits.json').then((response)=>response.json()).then((data)=> {
-  data.forEach((obj)=>{console.log(obj.fruit)});
+  data.forEach((obj)=>{console.log('Fruit name =', obj.fruit)});
+  console.log('');
+  data.filter((fruit)=>fruit.color==='Red').forEach((obj)=>{console.log('Only Red fruit - ', obj.fruit)});  
+  console.log('');
+  data.filter((fruit)=>fruit.size==='Large').forEach((obj)=>{console.log('Only Large fruit - ', obj.fruit)});
   
-  const onlyRed = data.filter((fruit)=>fruit.color==='Red');
-  console.log('Array only Red fruits', onlyRed);
-
-  const onlyLarge = data.filter((fruit)=>fruit.size==='Large');
-  console.log('Array only', onlyLarge);
+  console.log('');
+  data.filter((fruit)=>fruit.size==='Large'&&fruit.color==='Red').forEach((result)=>{console.log('Both filter:', result.fruit)})
 });
-
